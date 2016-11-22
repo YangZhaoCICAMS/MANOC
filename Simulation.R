568d46d1e0a0836e02d8ab4f1bda631fc3d6ddc4
chikinlam <cklamsta@connect.hku.hk>
2016-11-22 18:08:41 +0800

    Add files via upload

---
+++ Simulation.R
@@ -0,0 +1,54 @@
+# The function for conducting simulation study. # 
+source("PosteriorProbability.R")
+source("NextDoseComb.R")
+simulation <- function(simid, Tox_Prob_Mat, p.sample.mat, samplesize=60, cohortsize=3, target=0.3, alpha=0.35, delta=0.03, eta=0.60)
+{
+    Tox_Prob_Mat<-Tox_Prob_Mat[nrow(Tox_Prob_Mat):1,]
+	set.seed(simid)
+    print(c("simid",simid))
+	cohort <- samplesize/cohortsize
+	dlt <- rep(0, cohort)
+	dose.level <-array(0,dim=c(cohort+1,2))
+	dose.level[1, ] <- c(1,1)
+
+	ndose.A <- nrow(Tox_Prob_Mat)
+	ndose.B <- ncol(Tox_Prob_Mat)
+	y.mat <- array(0, dim=c(ndose.A, ndose.B))
+	n.mat <- array(0, dim=c(ndose.A, ndose.B))
+
+	for (i in 1:cohort)
+	{
+    	index <- dose.level[i,]
+		dlt[i] <- rbinom(1, cohortsize, Tox_Prob_Mat[index[1], index[2]]) 
+				
+		y.mat[index[1],index[2]] <- y.mat[index[1],index[2]] + dlt[i]
+		n.mat[index[1],index[2]] <- n.mat[index[1],index[2]] + cohortsize 
+		pos.model<-posteriorH(y=y.mat,n=n.mat,target=target,p.sample.mat=p.sample.mat)$lik
+		
+		pos.model.modified<-pos.model
+		pos.model.modified[which(n.mat==0,arr.ind=TRUE)]<-pos.model.modified[which(n.mat==0,arr.ind=TRUE)]+delta
+		
+		if (sum(dlt)==0)
+		{
+			dose.level[i+1,]<-c(min(ndose.A,index[1]+1),min(ndose.B,index[2]+1))
+		} 
+		else	
+		{
+            dose.level[i+1,] <-get.next.manoc(y=y.mat,n=n.mat,target=target,p.sample.mat=p.sample.mat,delta=delta,j_curr=index[1],k_curr=index[2],alpha=alpha,eta=eta)$next.dose
+		}
+	}
+	
+	### MTD Selection. ####
+	pos.model.original<-pos.model
+	pos.model<-pos.model*(n.mat>0)
+	MTD.comb<-which(pos.model==max(pos.model),arr.ind=TRUE)
+    MTD.sel<-Tox_Prob_Mat[MTD.comb]
+    
+    y.mat<-y.mat[nrow(y.mat):1,]
+    n.mat<-n.mat[nrow(n.mat):1,]
+    pos.model<-pos.model[nrow(pos.model):1,]
+    pos.model.original<-pos.model.original[nrow(pos.model.original):1,]
+    Tox_Prob_Mat<-Tox_Prob_Mat[nrow(Tox_Prob_Mat):1,]
+
+return(list(MTD.sel=MTD.sel,MTD.comb=MTD.comb,y.mat=y.mat,n.mat=n.mat,dose.level.explored=dose.level[1:cohort,],pos.model=pos.model,pos.model.original=pos.model.original,Tox_Prob_Mat=Tox_Prob_Mat))
+}
---
+++ ToxProb_Generate.R
@@ -0,0 +1,141 @@
+#### This is a function for generating samples of the toxicity matrix p from the joint prior distribution.
+
+generate_p.sample.mat<-function(ndose.A,ndose.B, NN=50000, target, epi)
+{
+  ll<-0.00
+  uu<-0.80
+  p.sample.mat<-array(NA,dim=c(NN,ndose.A,ndose.B,ndose.A,ndose.B)) 
+  for(j in 1:ndose.A)
+  {
+    for(k in 1:ndose.B)
+    {
+      # Under model M_{j,k}.
+      p.sample.mat[1:NN,j,k,j,k]<-runif(NN,target-epi,target+epi)
+      
+      #### For k'=k. ####
+      if (j>1)
+      {
+        for (l_j in (j-1):1)
+        {
+         	p.sample.mat[1:NN,l_j,k,j,k]<-sapply(1:NN,function(nn) 
+            runif(1,ll,min(p.sample.mat[nn,l_j+1,k,j,k],target-epi))
+          )
+        }
+      }
+      
+      if (j<ndose.A)
+      {
+        for (l_j in (j+1):ndose.A)
+        {
+         p.sample.mat[1:NN,l_j,k,j,k]<-sapply(1:NN,function(nn)
+            runif(1,max(p.sample.mat[nn,l_j-1,k,j,k],target+epi),uu)
+          )
+        }
+      }
+      
+      #### j'=j ####
+      if (k>1)
+      {
+        for (l_k in (k-1):1)
+        {
+        	p.sample.mat[1:NN,j,l_k,j,k]<-sapply(1:NN,function(nn) runif(1,ll,min(p.sample.mat[nn,j,l_k+1,j,k],target-epi))
+          )
+        }
+      }
+      
+      if (k<ndose.B)
+      {
+        for (l_k in (k+1):ndose.B)
+        {
+         	p.sample.mat[1:NN,j,l_k,j,k]<-sapply(1:NN,function(nn) runif(1,max(p.sample.mat[nn,j,l_k-1,j,k],target+epi),uu)
+          )
+        }
+      }
+      
+      # For elements not on j-th row and not on k-th column. 
+      if (j>1)
+      {
+        for (l_j in (j-1):1)
+        {
+          if (k>1)
+          {
+            for (l_k in (k-1):1)
+            {
+              ub <- sapply(1:NN, function(nn) min(p.sample.mat[nn,l_j+1, l_k, j, k], p.sample.mat[nn,l_j, l_k+1, j, k]))              
+              p.sample.mat[1:NN,l_j,l_k,j,k]<-sapply(1:NN,function(nn) runif(1,ll,ub[nn]))
+            }
+          }
+          
+          if (k<ndose.B)
+          {
+            for (l_k in (k+1):ndose.B)
+            {
+              lb<-sapply(1:NN, function(nn) p.sample.mat[nn,l_j,l_k-1,j, k])
+              ub<-sapply(1:NN, function(nn) p.sample.mat[nn,l_j+1,l_k,j, k])
+
+              p.sample.mat[1:NN,l_j,l_k,j,k] <- runif(NN,lb,ub)
+              vector<-sapply(1:NN, function(nn) return((lb[nn]<target-epi)&&(ub[nn]>target+epi)))
+              index<-which(vector==1)
+              
+              if (length(index)>0)
+              {
+              	for (i in 1:length(index))
+              	{
+               		prob<-c(target-epi-lb[index[i]],ub[index[i]]-target-epi)
+                	indicator<-sample(x=c(-1,1),size=1,replace=T,prob=prob/sum(prob))
+                	if(indicator==-1)
+                	{p.sample.mat[index[i],l_j,l_k,j,k]<-runif(1,lb[index[i]],target-epi)}
+                	else
+                	{p.sample.mat[index[i],l_j,l_k,j,k]<-runif(1,target+epi,ub[index[i]])}
+              	}	
+              }	
+            }
+          }
+        }
+      }
+      
+      if (j<ndose.A)
+      {
+        for (l_j in (j+1):ndose.A)
+        {
+          if (k>1)
+          {
+            for (l_k in (k-1):1)
+            {
+              lb<-sapply(1:NN,function(nn) p.sample.mat[nn,l_j-1,l_k,j,k])
+              ub<-sapply(1:NN,function(nn) p.sample.mat[nn,l_j,l_k+1,j,k])
+              
+              p.sample.mat[1:NN,l_j,l_k,j,k] <- runif(NN,lb,ub)
+              vector<-(sapply(1:NN, function(nn) return((lb[nn]<target-epi)&&(ub[nn]>target+epi))))
+              
+              index<-which(vector==1)
+              
+              if (length(index)>0)
+              {
+              	for (i in 1:length(index))
+              	{
+               		prob<-c(target-epi-lb[index[i]],ub[index[i]]-target-epi)
+                	indicator<-sample(x=c(-1,1),size=1,replace=T, prob=prob/sum(prob))
+                	if(indicator==-1)
+                	{p.sample.mat[index[i],l_j,l_k,j,k]<-runif(1,lb[index[i]],target-epi)}
+                	else
+                	{p.sample.mat[index[i],l_j,l_k,j,k]<-runif(1,target+epi,ub[index[i]])}
+              	}
+              }
+            }
+          }
+          
+          if (k<ndose.B)
+          {
+            for (l_k in (k+1):ndose.B)
+            {
+              lb<-sapply(1:NN, function(nn) max(p.sample.mat[nn,l_j-1,l_k, j, k],p.sample.mat[nn,l_j,l_k-1,j,k]))	
+              p.sample.mat[1:NN,l_j,l_k,j,k]<-sapply(1:NN,function(nn) runif(1,lb[nn],uu))							
+            }
+          }
+        }
+      }
+    }
+  }
+  return(p.sample.mat=p.sample.mat)
+}
